3da9f174ef709e8aa4cdbc215c198497c6833f3a
jenin <jeningeorge07@gmail.com>
2024-08-06 22:49:26 +0530

    basic_vite

--- basic_vite/src/App.jsx
+++ basic_vite/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Free  from './free'
 
 function App() {
+  const user = "hello world"
 
   return (
 
     // how to warp more than one element in react
     // <> are known as fragments 
+    // {user} this is known as evaluated expression
     <> 
-      <h1>world</h1>
+      <h1> {user}</h1>
       <Free/>
       <p>hello</p>
     </>
--- basic_vite/src/main.jsx
+++ basic_vite/src/main.jsx
@@ -3,8 +3,41 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  )
+}
+
+// doesn't work because the element will not be supported in the render scope as it has keys and values,
+// const reactELement = {
+//   type: 'a',
+//   props: {
+//       href: "https://google.com",
+//       target: '_blank'
+//   },
+//   children: 'click me to visit google'
+// }
+
+// make another const
+const anotherElement = "chai aur code" 
+// (
+  // <a href='https://www.google.com' target = '_blank'>visit google</a>
+// )
+
+// if you want to create reactelement without using html then you can create like this way
+const reactELement = React.createElement(
+  'a',
+  {href: 'https://www.google.com', target: '_blank'},
+  "click me to visit ",
+  anotherElement
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // calling the const in the render scope
+  // anotherElement
+  reactELement
+  // < App />
 )

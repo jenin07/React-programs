f7719c088b4c3b6b3b550d4535e7e26ea45a0e7e
jenin <jeningeorge07@gmail.com>
2024-08-06 21:44:32 +0530

    customreact

---
+++ custom_react/customreact.js
@@ -0,0 +1,20 @@
+function customrender(reactELement, container){
+    const domElement = document.createElement(reactELement.type)
+    domElement.innerHTML = reactELement.children;
+    domElement.setAttribute('href', reactELement.props.href);
+    domElement.setAttribute('target', reactELement.props.target);
+    container.appendChild(domElement)
+}
+
+const reactELement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: '_blank'
+    },
+    children: 'click me to visit google'
+}
+
+const container = document.querySelector("#root")
+
+customrender(reactELement, container)

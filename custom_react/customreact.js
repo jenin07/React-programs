00287369ce27cac8aa93f4b739a776f894a2bdf6
jenin <jeningeorge07@gmail.com>
2024-08-06 21:53:08 +0530

    customreact

--- custom_react/customreact.js
+++ custom_react/customreact.js
@@ -1,9 +1,22 @@
 function customrender(reactELement, container){
-    const domElement = document.createElement(reactELement.type)
+
+    // const domElement = document.createElement(reactELement.type)
+    // domElement.innerHTML = reactELement.children;
+    // domElement.setAttribute('href', reactELement.props.href);
+    // domElement.setAttribute('target', reactELement.props.target);
+    // container.appendChild(domElement)
+
+    const domElement = document.createElement(reactELement.type);
     domElement.innerHTML = reactELement.children;
-    domElement.setAttribute('href', reactELement.props.href);
-    domElement.setAttribute('target', reactELement.props.target);
+    for (const prop in reactELement.props){
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop, reactELement.props[prop])
+    }
+
     container.appendChild(domElement)
+
+
+
 }
 
 const reactELement = {

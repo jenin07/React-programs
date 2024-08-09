2b06921046b1d55a66185290c93f160648b22530
jenin <jeningeorge07@gmail.com>
2024-08-09 22:50:36 +0530

    props

--- tailwind_props/src/App.jsx
+++ tailwind_props/src/App.jsx
@@ -6,11 +6,18 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let obj = {
+    user: 'Jenin',
+    age: '21'
+  }
+
+  let arr = [2,3,4];
 
   return (
     <>
      <h1 className="bg-red-500 text-black p-4 rounded-md mb-4" >tailwind test</h1>
-     <Card/>
+     <Card username='Jenin' myobj = {obj} myarr ={arr}/>
+     <Card username='Jaim' btnText = "Hello from jaim"/>
     </>
   )
 }

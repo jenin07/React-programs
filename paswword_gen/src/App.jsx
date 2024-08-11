e0b1d314f867ae3370acd957a28955761b458b70
jenin <jeningeorge07@gmail.com>
2024-08-11 23:20:04 +0530

    project_2

--- paswword_gen/src/App.jsx
+++ paswword_gen/src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import { useState, useCallback  } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setlength] = useState(8);
+  const [number, setnumber] = useState(false);
+  const [character, setcharacter] = useState(false);
+  const [Password, setpassword] = useState("")
+
+  const password_gen = useCallback(() => {}, [Password, length, character, number])
 
   return (
     <>
-
+      <h1 className='text-4xl text-center text text-white'>
+        Password Generator
+      </h1>
     </>
   )
 }
